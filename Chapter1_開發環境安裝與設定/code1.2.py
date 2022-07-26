f126d90ef269f1dfcdc7ce2ac7a38c9a795f31da
jalenkai <jalenshiu@gmail.com>
2022-07-26 16:38:47 +0800

    Create code1.2.py

---
+++ Chapter1_開發環境安裝與設定/code1.2.py
@@ -0,0 +1,15 @@
+#範例1.2
+#使用type()檢查變數的型態
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+f = "我愛Python"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+#
